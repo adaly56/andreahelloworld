eff610ac0c38de8bc2f9998f40eaddb3d3a00a32
Andrea Daly <andie.daly@gmail.com>
2014-10-27 17:02:24 +0100

    My first commit

--- app/view/Home.js
+++ app/view/Home.js
@@ -26,7 +26,7 @@ Ext.define('HelloWorld.view.Home', {
             {
             	
                 html: [
-                       '<div class="block"><div class="centered"><div class="myHomeContainer" style="width: 900px; margin-left: auto; margin-right: auto;"><iframe width="100%" height="800px" frameBorder="0" style="-webkit-transform:scale(0.6);	 -moz-transform: scale(0.6); " class="noScrolling" scrolling="no" seamless="seamless" src="http://ladybugdesign.co.uk/slidingpuzzle/index.html"></iframe></div></div></div>'
+                       '<div class="myHomeContainer" style="width: 1000px; margin-left: auto; margin-right: auto;"><iframe width="100%" height="800px" frameBorder="0" style="-webkit-transform:scale(0.6);	 -moz-transform: scale(0.6); " class="noScrolling" scrolling="no" seamless="seamless" src="http://ladybugdesign.co.uk/slidingpuzzle/index.html"></iframe></div>'
                       // '<iframe width="100%" height="800px" class="noScrolling" scrolling="no" seamless="seamless" src="http://ladybugdesign.co.uk/slidingpuzzle/index.html"></iframe>'
                      //  '<div id="tops"><iframe width="100%" height="800px" frameBorder="0" style="-webkit-transform:scale(0.7);	 -moz-transform: scale(0.7); " class="noScrolling" scrolling="no" seamless="seamless" src="http://ladybugdesign.co.uk"></iframe></div>'
                        //	"<div id='div1' class='myCls'>hi</div>",
